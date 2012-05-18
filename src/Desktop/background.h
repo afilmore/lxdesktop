6ff847490a13ed5fa4f5b828f86cdf6d9746ede2
Axel FILMORE <axel.filmore@gmail.com>
2012-05-18 21:41:16 +0200

    Add Header...

--- src/Desktop/background.h
+++ src/Desktop/background.h
@@ -1,7 +1,18 @@
-
-
-
-
+/***********************************************************************************************************************
+ * background.h
+ * 
+ * Copyright 2012 Axel FILMORE <axel.filmore@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2.
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ * 
+ * This software is an experimental fork of PcManFm originally written by Hong Jen Yee aka PCMan for LXDE project.
+ * 
+ * Purpose: 
+ * 
+ * 
+ **********************************************************************************************************************/
 #ifndef __BACKGROUND_H__
 #define __BACKGROUND_H__
 
