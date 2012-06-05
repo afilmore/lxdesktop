6867baa02c53c62d6bdb94fd74fc65cea20287a8
Axel FILMORE <axel.filmore@gmail.com>
2012-06-05 05:23:06 +0200

    TreeModel: cleanup...

--- src/gtk/fm-dir-tree-model.c
+++ src/gtk/fm-dir-tree-model.c
@@ -1,26 +1,26 @@
 /***********************************************************************************************************************
  * 
- * fm-dir-tree-model.c
+ *      fm-dir-tree-model.c
  * 
- * Copyright 2010 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
- * Copyright 2012 Axel FILMORE <axel.filmore@gmail.com>
+ *      Copyright 2010 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *      Copyright 2012 Axel FILMORE <axel.filmore@gmail.com>
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
  * 
- * Purpose: 
+ *      Purpose: 
  * 
  * 
  * 
@@ -143,7 +143,7 @@ static void fm_dir_tree_model_init (FmDirTreeModel *model)
     
     // Check Subdirectories...
     model->check_subdir = TRUE;
-    model->check_subdir_mode2 = TRUE;
+//    model->check_subdir_mode2 = TRUE;
     
     g_queue_init (&model->subdir_checks);
     model->subdir_checks_mutex = g_mutex_new ();
