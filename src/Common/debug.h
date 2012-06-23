bd24f9b0a616e42410ade3d3dafa33f0f3719e2e
Axel FILMORE <axel.filmore@gmail.com>
2012-06-23 20:47:17 +0200

    Move back widgets to the library...

--- src/Common/debug.h
+++ src/Common/debug.h
@@ -28,11 +28,11 @@
 
 G_BEGIN_DECLS
 
-#ifdef ENABLE_TREEVIEW_DEBUG
-#define TREEVIEW_DEBUG(...) DEBUG(__VA_ARGS__)
-#else
-#define TREEVIEW_DEBUG(...)
-#endif
+//~ #ifdef ENABLE_TREEVIEW_DEBUG
+//~ #define TREEVIEW_DEBUG(...) DEBUG(__VA_ARGS__)
+//~ #else
+//~ #define TREEVIEW_DEBUG(...)
+//~ #endif
 
 
 G_END_DECLS
